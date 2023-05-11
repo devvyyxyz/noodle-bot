5856e27e8037a61e99b52cbd94d6e04152dda0bd
devvyyxyz <132696257+devvyyxyz@users.noreply.github.com>
2023-05-11 04:33:40 +0000

    update

--- index.js
+++ index.js
@@ -1,13 +1,3 @@
-/* Setup
-
-   1. Create a .env file (click add file then remane it to .env)
-
-   2. Put "token=" (without quotes) into the .env file followed by your Discord Bot token (No spaces!)
-
-*/
-
-/* If you use uptimerobot to ping, delete this line and line 20
-
 const express = require("express");
 const app = express();
 
@@ -16,8 +6,6 @@ app.listen(() => console.log("Server started"));
 app.use('/ping', (req, res) => {
   res.send(new Date());
 });
-
-*/
 
 const Discord = require('discord.js');
 const client = new Discord.Client();
