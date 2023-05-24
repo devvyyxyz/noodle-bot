715bc9d326359407bf717ce9b2310a1283908c50
devvyyxyz <132696257+devvyyxyz@users.noreply.github.com>
2023-05-24 19:15:29 +0000

    update

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
+  console.log(`Logged in as: ${client.user.tag}!`);
 });
 
 client.on('message', message => {
